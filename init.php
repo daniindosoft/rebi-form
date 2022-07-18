57ba6b04fa68c7c92b2dca7eb132280c3421606a
mcdani pc <danisaputraakatsuki@gmail.com>
2022-07-18 21:49:36 +0700

    set init dulu

---
+++ init.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: REBI - Form
+Description: Membuat custom form untuk kebutuhan bisnis anda, seperti form untuk lead, membuat form untuk order
+Version: 1
+Author: REBI - RemoteBisnis
+Author URI: http://RemoteBisnis.com/tools
+*/
+// function to create the DB / Options / Defaults					
+
+define('RDRFO', plugin_dir_path(__FILE__)); 
+
+// type form
+// - 
